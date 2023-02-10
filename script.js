078081335977bb55630ffb68017b24785d734e5f
Ângelo Ferreira <112225868+angelloferreira@users.noreply.github.com>
2023-02-09 20:44:39 -0400

    Commit start

---
+++ script.js
@@ -0,0 +1,17 @@
+let lampadas = document.querySelectorAll('img');
+
+function lumos(event){
+
+    if(event.currentTarget.getAttribute('src')=='img/off.png'){
+        event.currentTarget.src = 'img/on.png';
+    }else{
+        event.currentTarget.src = 'img/off.png';
+    }
+}
+
+lampadas.forEach((lampada) => {
+    lampada.addEventListener('click', lumos);
+});
+
+    
+    
